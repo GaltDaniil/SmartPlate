b441bd8deed2132f99c33982aae088d648c69357
DaniilGalt <94684997+GaltDaniil@users.noreply.github.com>
2023-04-04 07:17:47 +0800

    2

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
     return (
         <>
             <p>{JSON.stringify(tg.initDataUnsafe)}</p>
+            <p>{JSON.stringify(tg.initDataUnsafe)}</p>
             <Router>
                 <Routes>
                     <Route path="/" element={<Main />} />
