0aabea9c3a6c4410395c4acb4ad3cbda866f984a
DaniilGalt <94684997+GaltDaniil@users.noreply.github.com>
2023-04-03 08:44:35 +0800

    redirect and router

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div className="container">
+            <img src="" alt="" />
+            <div className="shape"></div>
+        </div>
+    );
+};
